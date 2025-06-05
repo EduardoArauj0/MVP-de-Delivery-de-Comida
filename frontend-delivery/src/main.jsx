f6719727ac9d701eb92285ca8039059f4e35e1f3
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-06-05 16:06:17 -0300

    fix: corrige erro de useNavigate fora do Router e exportação do App.jsx

--- frontend-delivery/src/main.jsx
+++ frontend-delivery/src/main.jsx
@@ -7,15 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { CartProvider } from './context/CartContext.jsx';
 import { AppDataProvider } from './context/AppDataContext.jsx';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './router/routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <AppDataProvider>
-          <App />
-        </AppDataProvider>
-      </CartProvider>
-    </AuthProvider>
-  </React.StrictMode>,
+    <RouterProvider router={routes}>
+      <AuthProvider>
+        <CartProvider>
+          <AppDataProvider>
+            <App />
+          </AppDataProvider>
+        </CartProvider>
+      </AuthProvider>
+    </RouterProvider>
+  </React.StrictMode>
 );
