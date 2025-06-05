4790c47b58dababbcda496c62f890b7f6c51f9db
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-06-04 23:03:47 -0300

    fix: Garante o correto fornecimento de imagens estáticas da pasta uploads

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,36 +5,32 @@ const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 
 app.use(cors());
-
 app.use(express.json());
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 const restauranteRoutes = require('./routes/restauranteRoutes');
-app.use('/restaurantes', restauranteRoutes);
-
 const produtoRoutes = require('./routes/produtoRoutes');
-app.use('/produtos', produtoRoutes);
-
 const pedidoRoutes = require('./routes/pedidoRoutes');
-app.use('/pedidos', pedidoRoutes);
-
 const avaliacaoRoutes = require('./routes/avaliacaoRoutes');
-app.use('/avaliacoes', avaliacaoRoutes);
-
 const carrinhoRoutes = require('./routes/carrinhoRoutes');
-app.use('/carrinho', carrinhoRoutes);
-
 const modoPagamentoRoutes = require('./routes/modoPagamentoRoutes');
-app.use('/modos-pagamento', modoPagamentoRoutes);
-
 const userRoutes = require('./routes/userRoutes');
-app.use('/users', userRoutes);
-
 const uploadRoutes = require('./routes/uploadRoutes');
-app.use('/upload', uploadRoutes);
+const cozinhaRoutes = require('./routes/cozinhaRoutes');
 
-const swaggerSpec = require('./config/swagger'); 
+app.use('/restaurantes', restauranteRoutes);
+app.use('/produtos', produtoRoutes);
+app.use('/pedidos', pedidoRoutes);
+app.use('/avaliacoes', avaliacaoRoutes);
+app.use('/carrinho', carrinhoRoutes);
+app.use('/modospagamento', modoPagamentoRoutes);
+app.use('/users', userRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/cozinhas', cozinhaRoutes);
+
+// Swagger Docs
+const swaggerSpec = require('./config/swagger');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (req, res) => res.send('API Delivery online 🚀'));
